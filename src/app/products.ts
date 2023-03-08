6c40925e3b7d379d494a1fd18f3d2119ccbd94f3
TienLe0305 <TienLe0305>
2023-03-09 03:33:28 +0700

    deploy commit

---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Phone 14',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      id: 2,
+      name: 'Phone 12',
+      price: 750,
+      description: 'A great phone with one of the best cameras'
+    },
+    {
+      id: 3,
+      name: 'Phone X',
+      price: 299, 
+      description: 'A best phone with two of the best cameras'
+    }
+  ];
+  
+  
